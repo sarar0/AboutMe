6536781bb566fd2a93deea8112fd26d09bed7ad6
Sara <srancati0@gmail.com>
2019-04-29 17:17:27 +0100

    Image slide and style

--- home.js
+++ home.js
@@ -1,14 +1,60 @@
-var slideIndex = 0;
-carousel();
+var slideIndex = 1;
 
-function carousel() {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none"; 
-  }
-  slideIndex++;
-  if (slideIndex > x.length) {slideIndex = 1} 
-  x[slideIndex-1].style.display = "block"; 
-  setTimeout(carousel, 2000); // Change image every 2 seconds
+function plusSlides(n) {
+  slideIndex = slideIndex += n;
+  showSlide(slideIndex);
 }
+
+showSlide(1);
+
+function showSlide(n) {
+  var i;
+  var slides = document.getElementsByClassName("slides");
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
+
+autoShow();
+
+function autoShow() {
+  var i;
+
+  var slides = document.getElementsByClassName("slides");
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
+  slideIndex++;
+  setTimeout(autoShow, 3000);
+}
+
+document.onkeydown = function(e) {
+  switch (e.keyCode) {
+    case 37:
+      e.preventDefault();
+      slideIndex--;
+      showSlide(slideIndex);
+      break;
+    case 39:
+      e.preventDefault();
+      slideIndex++;
+      showSlide(slideIndex);
+      break;
+  }
+};
+
